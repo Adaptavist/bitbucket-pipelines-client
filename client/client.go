dea34bb0c58619a28920f04fca671214dfb216cd
Dragos Ciupureanu <dciupureanu@adaptavist.com>
2021-10-21 12:59:30 +0100

    fix(error): pass down the error from the bitbucket client

--- client/client.go
+++ client/client.go
@@ -51,6 +51,10 @@ func (h Client) RunPipeline(request model.PostPipelineRequest) (pipeline *model.
 			Repository: request.Repository,
 			Pipeline:   pipeline,
 		})
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return
@@ -126,7 +130,7 @@ func (h Client) GetTag(request model.GetTagRequest) (response *model.TagResponse
 	if err != nil {
 		return nil, err
 	}
-	url:= fmt.Sprintf("%s/2.0/repositories/%s/%s/refs/tags/%s", h.Config.GetBaseURL(), *w, *r, request.Tag)
+	url := fmt.Sprintf("%s/2.0/repositories/%s/%s/refs/tags/%s", h.Config.GetBaseURL(), *w, *r, request.Tag)
 	err = h.getUnmarshalled(url, &response)
 	return
 }
